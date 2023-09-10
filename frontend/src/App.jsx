1c130b7feeeb91361edbffc5f833e964caeca979
Joseph Ruiz <ruizdev7@outlook.com>
2023-09-10 14:17:51 +0200

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+} from "react-router-dom"
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+
+	return (
+		<Navbar />
+	)
+}
+
+export default App
