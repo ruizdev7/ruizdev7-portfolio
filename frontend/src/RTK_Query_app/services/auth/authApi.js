a5685f9824196c9aa70d707943820b0c9c45a286
Joseph Ruiz <ruizdev7@outlook.com>
2025-02-02 17:34:38 +0100

    Refactor Docker Compose and frontend setup; update service configurations, expose correct ports, and adjust API base URL, funcional !!!!!

--- frontend/src/RTK_Query_app/services/auth/authApi.js
+++ frontend/src/RTK_Query_app/services/auth/authApi.js
@@ -3,9 +3,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:5000/api/v1",
+    baseUrl: "http://127.0.0.1:6000/api/v1",
     prepareHeaders: (headers, { getState }) => {
-      const token = getState("authApi.current_user.token");
+      const token = getState().authApi.current_user?.token;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
       headers.set("Access-Control-Allow-Origin", "*");
       return headers;
     },
