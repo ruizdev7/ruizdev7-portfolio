0034af68295865b3305bb2f5a77da688f4f0aa01
Joseph Ruiz <ruizdev7@outlook.com>
2025-03-24 10:53:54 +0100

    Refactor configuration management; enhance environment variable handling and add JWT settings for improved security and flexibility

--- backend/portfolio_app/config.py
+++ backend/portfolio_app/config.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 
 # Cargar las variables de entorno desde el archivo .env correspondiente
-env = os.getenv("FLASK_ENV", "development")
+env = os.getenv(
+    "FLASK_ENV",
+)
 
 if env == "production":
     load_dotenv(dotenv_path=".env.production")
@@ -24,19 +26,33 @@ def get_database_uri():
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = get_database_uri()
-    # ...otras configuraciones...
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
+    JWT_REFRESH_TOKEN_EXPIRES = 3600
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
+    CORS_HEADERS = "Content-Type"
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    FLASK_DEBUG = 1
+    os.environ["FLASK_DEBUG"] = "1"
     SQLALCHEMY_DATABASE_URI = get_database_uri()
 
 
 class TestingConfig(Config):
+    DEBUG = False
+    FLASK_DEBUG = 0
+    os.environ["FLASK_DEBUG"] = "0"
     TESTING = True
     SQLALCHEMY_DATABASE_URI = get_database_uri()
 
 
 class ProductionConfig(Config):
+    DEBUG = False
+    FLASK_DEBUG = 0
+    os.environ["FLASK_DEBUG"] = "0"
+    TESTING = True
     SQLALCHEMY_DATABASE_URI = get_database_uri()
-    # ...otras configuraciones...
