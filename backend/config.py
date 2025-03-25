dad5b4d2b42d064978b41741c2a8ae58ba978f4b
Joseph Ruiz <ruizdev7@outlook.com>
2025-03-25 12:16:55 +0100

    Refactor configuration management; streamline environment variable usage for database and secret key settings

--- backend/config.py
+++ backend/config.py
@@ -8,30 +8,24 @@ load_dotenv()  # Load environment variables from .env file
 class Config(object):
     DEBUG = False
     TESTING = False
-    DB_NAME = os.getenv("DB_NAME", "portfolio_app_pro")
-    DB_USERNAME = os.getenv("DB_USERNAME", "root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USERNAME = os.getenv("DB_USERNAME")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
     SESSION_COOKIE_SECURE = True
-    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "SQLALCHEMY_DATABASE_URI",
-        "mysql+pymysql://root:root@127.0.0.1:3306/portfolio_app_dev",
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 
 # PRODUCTION SERVER CONFIGURATION
 class ProductionConfig(Config):
     DEBUG = False
     SESSION_COOKIE_SECURE = True
-    SECRET_KEY = os.getenv("PROD_SECRET_KEY", "super-secret-key")
-    DB_NAME = os.getenv("PROD_DB_NAME", "portfolio_app_pro")
-    DB_USERNAME = os.getenv("PROD_DB_USERNAME", "root")
-    DB_PASSWORD = os.getenv("PROD_DB_PASSWORD", "root")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "PROD_SQLALCHEMY_DATABASE_URI",
-        "mysql+pymysql://root:password@mysql_db:3306/portfolio_app_pro",
-    )
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USERNAME = os.getenv("DB_USERNAME")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 
 # DEVELOPMENT SERVER CONFIGURATION
@@ -44,11 +38,8 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
     SESSION_COOKIE_SECURE = False
-    SECRET_KEY = os.getenv("TEST_SECRET_KEY", "testsecretkey")
-    DB_NAME = os.getenv("TEST_DB_NAME", "portfolio_app_test")
-    DB_USERNAME = os.getenv("TEST_DB_USERNAME", "root")
-    DB_PASSWORD = os.getenv("TEST_DB_PASSWORD", "root")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "TEST_SQLALCHEMY_DATABASE_URI",
-        "mysql+pymysql://root:root@127.0.0.1:3306/portfolio_app_test",
-    )
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USERNAME = os.getenv("DB_USERNAME")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
